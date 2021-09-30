bfde302eb6cf2c49a03e5b69c4258e42977b3854
johnconnor <zkmd2012@inbox.ru>
2021-09-30 17:11:22 +0500

    03. User Schema is created with different types of vilidation properties (vid. 4)

--- #controllers/authController.js
+++ #controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../#models/User')
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
@@ -6,10 +8,24 @@ module.exports.login_get = (req, res) => {
     res.render('login')
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send('new signup')
+module.exports.signup_post = async (req, res) => {
+
+    const {email, password} = req.body
+    
+    try {
+        const user = await User.create({ email, password,}) //USer created and saved in the database
+        res.status(201).json(user)
+
+    } catch (error) {
+        console.log(err)
+        res.status(400).send('error, user not created')
+    }
 }
 
-module.exports.login_post = (req, res) => {
-    res.render('signup')
+module.exports.login_post = async (req, res) => {
+    
+    const {email, password} = req.body
+    console.log(email, password)
+
+    res.send('user login')
 }
