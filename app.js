cc00cc2ce51e21154c5b908419af40091301ec37
johnconnor <zkmd2012@inbox.ru>
2021-09-30 15:54:33 +0500

    02. Core routes are created. Logic for routes and controlles is separated into different folders

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./#routes/authRoutes')
 
-const app = express();
+const app = express()
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // database connection
 const dbURI = 'mongodb+srv://kirill:kirill@cluster0.w7dqn.mongodb.net/jwt_netninja?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }, () => console.log('DB connected'))
   .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', (req, res) => res.render('smoothies'))
+
+app.use(authRoutes)
