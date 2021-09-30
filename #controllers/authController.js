f2faab5add451cb2ee9e8c98dab9e1d2f161cb90
johnconnor <zkmd2012@inbox.ru>
2021-09-30 17:38:50 +0500

    04. Start working on Mongoose Validation. Instalation of Validate library

--- #controllers/authController.js
+++ #controllers/authController.js
@@ -1,5 +1,10 @@
 const User = require('../#models/User')
 
+//handle errors
+const handleErrors = err => {
+    console.log(err.message, err.code)
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
@@ -17,7 +22,7 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user)
 
     } catch (error) {
-        console.log(err)
+        handleErrors(err)
         res.status(400).send('error, user not created')
     }
 }
