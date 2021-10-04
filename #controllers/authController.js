41da73c7b9b826ef4d3d1005610b017014e10f54
johnconnor <zkmd2012@inbox.ru>
2021-10-04 14:07:59 +0500

    04.1 Validation finished

--- #controllers/authController.js
+++ #controllers/authController.js
@@ -1,8 +1,30 @@
 const User = require('../#models/User')
 
 //handle errors
-const handleErrors = err => {
+const handleErrors = (err) => {
     console.log(err.message, err.code)
+
+    let errors = {
+        email: '',
+        password: '',
+    } 
+    
+    //duplicate error code
+    if (err. code === 11000) {
+        errors.email = 'This email is already registered'
+        return errors
+    }
+
+    //validation errors
+    if(err.message.includes('user validation failed')) {
+        console.log(Object.values(err.errors))  //returns an array of object values, without the keys of the object
+        
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message
+        })
+    }    
+
+    return errors
 }
 
 module.exports.signup_get = (req, res) => {
@@ -21,9 +43,9 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password,}) //USer created and saved in the database
         res.status(201).json(user)
 
-    } catch (error) {
-        handleErrors(err)
-        res.status(400).send('error, user not created')
+    } catch (err) {
+        const errors = handleErrors(err)
+        res.status(400).json(errors)
     }
 }
 
