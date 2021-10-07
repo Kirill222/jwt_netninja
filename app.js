60cc97bc275214bf645f7f377c4b18ef5bea9707
johnconnor <zkmd2012@inbox.ru>
2021-10-07 18:07:09 +0500

    08. Set and read cookie explained. Use of cookie-parser library explained

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./#routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs')
@@ -20,5 +22,18 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'))
 app.get('/smoothies', (req, res) => res.render('smoothies'))
-
 app.use(authRoutes)
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  //res.setHeader('Set-Cookie', 'newUser=true') //classic, old way to set cookie
+  res.cookie('newUser', false) //set cookie with cookie-parser library
+  res.cookie('isEmployee', true, {maxAge: 1000*60*60*24}) //the third argument is the time of cookie to exist
+  res.send('you got the cookie')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies)
+  res.json(cookies.newUser)
+})
